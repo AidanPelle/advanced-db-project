7cd3cea55e747db93b6f7d9e9cc5e6d303abc56d
Brett Semmler <brett.semmler@hotmail.com>
2024-10-26 12:48:54 -0600

    Add SensorData and Devices Controllers
    Introduced new API controllers for handling sensor data and device operations. Both controllers provide endpoints for CRUD operations, though some sensor data methods are yet to be fully implemented.

---
+++ api/shard-db/shard-db/controllers/DevicesController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace shard_db.controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DevicesController(ApplicationDbContext context) : ControllerBase
+{
+    [HttpGet("/{deviceId}")]
+    public async Task<ActionResult<Device>> GetDevice(int deviceId)
+    {
+        var device = await context.Device.FindAsync(deviceId);
+
+        if (device == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(device);
+    }
+
+    [HttpGet("/all")]
+    public async Task<ActionResult<List<Device>>> GetDevices()
+    {
+        var devices = await context.Device.ToListAsync();
+
+        return Ok(devices);
+    }
+
+    [HttpPost("/")]
+    public async Task<ActionResult<Device>> CreateDevice(Device device)
+    {
+        context.Device.Add(device);
+        await context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetDevice), new { deviceId = device.Id }, device);
+    }
+
+    [HttpPut("/{deviceId:int}")]
+    public async Task<ActionResult> UpdateDevice(int deviceId, Device updatedDevice)
+    {
+        var device = await context.Device.FindAsync(deviceId);
+
+        if (device == null)
+        {
+            return NotFound();
+        }
+
+        device.Name = updatedDevice.Name;
+        await context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("/{deviceId:int}")]
+    public async Task<ActionResult> DeleteDevice(int deviceId)
+    {
+        var device = await context.Device.FindAsync(deviceId);
+
+        if (device == null)
+        {
+            return NotFound();
+        } 
+
+        context.Device.Remove(device);
+        await context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
