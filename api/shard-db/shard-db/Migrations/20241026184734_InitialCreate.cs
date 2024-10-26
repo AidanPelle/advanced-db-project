70d84f60fdbde56a3d26bf508c938430e4e0626c
Aidan Pelle <aidan.pelle@workhub.com>
2024-10-26 12:49:05 -0600

    Add migration

---
+++ api/shard-db/shard-db/Migrations/20241026184734_InitialCreate.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace shard_db.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Device",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Device", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Site",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Site", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sensor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DeviceId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Units = table.Column<string>(type: "TEXT", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sensor", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sensor_Device_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Device",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fragment",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SiteId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fragment", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Fragment_Site_SiteId",
+                        column: x => x.SiteId,
+                        principalTable: "Site",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SensorData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SensorId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ReceivedTimestamp = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Value = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SensorData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SensorData_Sensor_SensorId",
+                        column: x => x.SensorId,
+                        principalTable: "Sensor",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "QueryLog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FragmentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SiteId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AccessDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DataType = table.Column<int>(type: "INTEGER", nullable: false),
+                    DataVolume = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QueryLog", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QueryLog_Fragment_FragmentId",
+                        column: x => x.FragmentId,
+                        principalTable: "Fragment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_QueryLog_Site_SiteId",
+                        column: x => x.SiteId,
+                        principalTable: "Site",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fragment_SiteId",
+                table: "Fragment",
+                column: "SiteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QueryLog_FragmentId",
+                table: "QueryLog",
+                column: "FragmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QueryLog_SiteId",
+                table: "QueryLog",
+                column: "SiteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sensor_DeviceId",
+                table: "Sensor",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SensorData_SensorId",
+                table: "SensorData",
+                column: "SensorId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QueryLog");
+
+            migrationBuilder.DropTable(
+                name: "SensorData");
+
+            migrationBuilder.DropTable(
+                name: "Fragment");
+
+            migrationBuilder.DropTable(
+                name: "Sensor");
+
+            migrationBuilder.DropTable(
+                name: "Site");
+
+            migrationBuilder.DropTable(
+                name: "Device");
+        }
+    }
+}
