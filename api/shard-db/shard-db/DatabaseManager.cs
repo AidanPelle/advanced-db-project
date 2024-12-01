ee863b7afafb9c2c27e6936c2de0411943cc0364
Brett Semmler <brett.semmler@hotmail.com>
2024-12-01 14:58:41 -0700

    Initialize device databases from JSON configuration
    Refactor DatabaseManager to dynamically initialize device databases by loading site data from AppConf.json using System.Text.Json. Implement migrations and conditional device data additions during initialization to ensure database consistency.

--- api/shard-db/shard-db/DatabaseManager.cs
+++ api/shard-db/shard-db/DatabaseManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 
 namespace shard_db;
@@ -7,9 +8,37 @@ public class DatabaseManager
 {
     DatabaseManager()
     {
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        dbContext.Database.Migrate();
+        
+        if (!dbContext.Device.Any())
+        {
+            var jsonData = File.ReadAllText("./AppConf.json");
+            var sites = JsonSerializer.Deserialize<List<SiteDto>>(jsonData);
+
+            var bkDbContextOptionsBuilder = new DbContextOptionsBuilder<BookKeepingDbContext>();
+            bkDbContextOptionsBuilder.UseSqlite("Data Source=./BookKeeping.db");
+            BookKeepingDbContext = new BookKeepingDbContext(bkDbContextOptionsBuilder.Options);
             
+            
+            foreach (var site in sites)
+            {
+                var deviceDbOptionsBuilder = new DbContextOptionsBuilder<DeviceDbContext>();
+                deviceDbOptionsBuilder.UseSqlite($"Data Source={site.Name}.db");
+                DeviceDbContexts.Add(new DeviceDbContext(deviceDbOptionsBuilder.Options));
+            }
+            
+            
+        
+            // Add data to the database
+            if (devices != null)
+            {
+                dbContext.Device.AddRange(devices);
+                dbContext.SaveChanges();
+            }
+        }
     }
     
     public BookKeepingDbContext BookKeepingDbContext { get; set; }
-    public List<DeviceDbContext> DeviceDbContexts { get; set; }
+    public List<DeviceDbContext> DeviceDbContexts { get; set; } = new List<DeviceDbContext>();
 }
