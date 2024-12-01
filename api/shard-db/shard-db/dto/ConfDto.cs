fd91c0cc22b0f6a4431445eca0a9d82a8265486b
Aidan Pelle <aidan.pelle@workhub.com>
2024-12-01 14:40:35 -0700

    created dto for json

---
+++ api/shard-db/shard-db/dto/ConfDto.cs
@@ -0,0 +1,16 @@
+public class SiteDto {
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public List<DeviceConfDto> Devices { get; set; } = null!;
+}
+
+public class DeviceConfDto {
+    public string Name { get; set; } = null!;
+    public int UUID { get; set;}
+    public List<SensorDto> Sensors { get; set; } = null!;
+}
+
+public class SensorDto {
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
